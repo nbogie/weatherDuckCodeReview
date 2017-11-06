3c605ffe216efa90906cdeb982dac42b852fd467
Neill <neillbogie@googlemail.com>
2017-11-06 23:02:20 +0000

    Add rough stub for GPIO when not running on a rPi

---
+++ fakeGPIO.py
@@ -0,0 +1,27 @@
+BCM='bcm'
+OUT='out'
+IN='in'
+PUD_UP='pud_up'
+
+def setwarnings(b):
+    return
+
+def setmode(numbering):
+    return
+
+def setup(num, mode, pull_up_down=PUD_UP):
+    return
+
+def output(num, state):
+    return
+
+class FakePWM():
+    def ChangeDutyCycle(self, duty):
+        return
+
+def PWM(a, b):
+    return FakePWM()    
+
+def input(num):
+    return True
+
