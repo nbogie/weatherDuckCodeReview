c14d4ee0bb98a25a74e88f15aa5f005eabd81ffc
Neill <neillbogie@googlemail.com>
2017-11-07 00:23:41 +0000

    placate autopep8

--- fakeGPIO.py
+++ fakeGPIO.py
@@ -1,27 +1,33 @@
-BCM='bcm'
-OUT='out'
-IN='in'
-PUD_UP='pud_up'
+BCM = 'bcm'
+OUT = 'out'
+IN = 'in'
+PUD_UP = 'pud_up'
+
 
 def setwarnings(b):
     return
 
+
 def setmode(numbering):
     return
+
 
 def setup(num, mode, pull_up_down=PUD_UP):
     return
 
+
 def output(num, state):
     return
+
 
 class FakePWM():
     def ChangeDutyCycle(self, duty):
         return
 
+
 def PWM(a, b):
-    return FakePWM()    
+    return FakePWM()
+
 
 def input(num):
     return True
-
